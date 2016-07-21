c26af098fe0abd461c7b77a075d35f22e83eaf5e
evilchiang <evilchiang@gmail.com>
2016-07-21 16:00:29 +0800

    quick update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,6 +25,22 @@ class GroupsController < ApplicationController
     end
   end
 
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "修改討論板成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "討論板已刪除"
+  end
+
   private
 
   def group_params
