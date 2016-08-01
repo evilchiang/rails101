3bbb70b7ef7bb96a40931997d51b18fd5cd8d243
evilchiang <evilchiang@gmail.com>
2016-08-01 19:14:06 +0800

    practice 2nd time, branch: new_2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "早安！你好！notice"
+    flash[:alert] = "早安！你好！alert"
+    flash[:warning] = "早安！你好！warning"
+  end
 end
